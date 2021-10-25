0a1a8112f5a212b0f697247862060f434cb58c2e
1023150697@qq.com <1023150697@qq.com>
2021-10-25 18:27:24 +0800

    update

--- app.php
+++ app.php
@@ -7,45 +7,9 @@ return [
           '@runtime' => __DIR__ . '/runtime',
           '@controllers' => '/www/wwwroot/eazy-basic/controllers',
       ],
-    'server' => [
-      0 => [
-        'name' => 's1',
-        'host' => '0.0.0.0',
-        'port' => 9502,
-        'setting' => [
-          'enable_static_handler' => false,
-          'document_root' => '/www/wwwroot/eazy-basic/web',
-          'worker_num' => 1,
-          'enable_coroutine' => true,
-          'hook_flags' => 2147481599,
-          'daemonize' => false,
-          'log_file' => '/www/wwwroot/eazy-basic/runtime/s1.log',
-          'pid_file' => '/www/wwwroot/eazy-basic/runtime/s1.pid',
-        ],
-      ],
-      1 => [
-        'name' => 's2',
-        'host' => '0.0.0.0',
-        'port' => 9503,
-        'setting' => [
-          'enable_static_handler' => false,
-          'document_root' => '/www/wwwroot/eazy-basic/web',
-          'worker_num' => 1,
-          'enable_coroutine' => true,
-          'hook_flags' => 2147481599,
-          'daemonize' => false,
-          'log_file' => '/www/wwwroot/eazy-basic/runtime/s2.log',
-          'pid_file' => '/www/wwwroot/eazy-basic/runtime/s2.pid',
-        ],
-      ],
-    ],
-    'config' => [
-      'aliases' => [
-        '@controllers' => '/www/wwwroot/eazy-basic/controllers',
-        '@app' => '/www/wwwroot/eazy-basic',
-      ],
       'bootstrap' => [
-          'urlManager'
+          'urlManager',
+          'log'
       ],
       'components' => [
           'urlManager' => [
@@ -55,7 +19,7 @@ return [
                   ['GET', '/', '@controllers/site/index'],
 
                   // Multiple request methods.
-                  [['POST', 'DELETE'], '/site/methods', '@controllers/site/methods'],
+                  [['POST', 'DELETE'], '/site/methods/{id:\d+}', '@controllers/site/methods'],
                   // set different routes to point to the same action.
                   [['GET', 'POST'], '/about.html', '@controllers/site/methods'],
 
@@ -66,7 +30,18 @@ return [
                   ],
               ]
           ],
-      ],
-    ],
+          'log' => [
+              'class' => \eazy\http\log\LogDispatcher::class,
+              'targets' => [
+                  'application' => [
+                      'microtime' => false,
+                      'logFile' => '@runtime/log/app.log'
+                  ],
+                  'debug' => [
+                      'logFile' => '@runtime/log/debug.log'
+                  ]
+              ]
+          ]
+      ]
   ],
 ];
