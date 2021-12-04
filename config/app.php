a3bfeac7d5354d789e202a7081b56128aab975db
1023150697@qq.com <1023150697@qq.com>
2021-12-04 22:52:49 +0800

    update

--- config/app.php
+++ config/app.php
@@ -2,13 +2,23 @@
 
 return [
     'aliases' => [
-        '@controllers' => APP_PATH . '/controllers'
+        '@controllers' => APP_PATH . '/controllers',
+        '@eazy' => APP_PATH . '/vendor/eazysoft/eazy-http/src',
+        '@app' => APP_PATH
     ],
     'components' => [
         'router' => [
             'route' => [
                 ['GET', '/', '@controllers/site/index'],
             ]
+        ],
+        'db' => [
+            'host' => '192.168.10.154',
+            'port' => 3306,
+            'dbname' => 'toom',
+            'charset' => 'utf8mb4',
+            'username' => 'root',
+            'password' => 'root',
         ]
     ],
 ];
